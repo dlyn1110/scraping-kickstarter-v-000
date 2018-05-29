0dab675147a0dce89395b39638230591d73229a9
Dani Lyn <dlyn1110@gmail.com>
2018-05-29 20:55:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,7 @@ binding.pry
  # title: project.css("h2.bbcard_name strong a").text
  # image link: project.css("div.project-thumbnail a img").attribute("src").value
  # description: project.css("p.bbcard_blurb").text
- #location: 
+ # location: project.css("ul.project-meta").text
+
 end
 create_project_hash
