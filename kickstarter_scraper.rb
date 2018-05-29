8bc187761d326676632e65a241475e5028896d3f
Dani Lyn <dlyn1110@gmail.com>
2018-05-29 21:00:54 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,11 @@ def create_project_hash
 
   projects = {}
 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects(project) = {}
+
+
 
  # projects: kickstarter.css("li.project.grid_4")
  # title: project.css("h2.bbcard_name strong a").text
